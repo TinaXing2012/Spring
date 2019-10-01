6da62331c4ae0f2c33753bc74947adc9372934fe
rxing <rujuanxing@gmail.com>
2019-10-01 17:01:23 -0500

    Spring MVC startcode - XML version

---
+++ springmvcxml/src/main/java/xing/rujuan/WelcomeController.java
@@ -0,0 +1,13 @@
+package xing.rujuan;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+
+    @RequestMapping("/")
+    public String welcome(){
+        return "/WEB-INF/jsp/welcome.jsp";
+    }
+}
