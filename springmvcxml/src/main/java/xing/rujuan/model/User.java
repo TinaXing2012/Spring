d834dc96fe3e4ce448e71b9b53d01454cc5b9eb9
rxing <rujuanxing@gmail.com>
2019-10-01 21:07:50 -0500

    DataBinding Basic Version

---
+++ springmvcxml/src/main/java/xing/rujuan/model/User.java
@@ -0,0 +1,31 @@
+package xing.rujuan.model;
+
+public class User {
+
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
