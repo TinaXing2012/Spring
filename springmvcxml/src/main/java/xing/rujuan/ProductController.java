fae8e73fc30e3b126509bfa00adffa2de8a813b4
rxing <rujuanxing@gmail.com>
2019-10-01 20:29:37 -0500

    @PathVariable

--- springmvcxml/src/main/java/xing/rujuan/ProductController.java
+++ springmvcxml/src/main/java/xing/rujuan/ProductController.java
@@ -1,21 +1,25 @@
 package xing.rujuan;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/product")
 public class ProductController {
 
 
-    //http://localhost:8080/product/listProducts?categoryId=1
+    //http://localhost:8080/product/listProducts?categoryId=1&categoryName=computer
     @GetMapping("/listProducts")
     public String getProductByCategory(@RequestParam Integer categoryId,
                                        @RequestParam(value = "categoryName", required = false, defaultValue = "laptop") String name) {
         System.out.println("Id: " + categoryId + " , Name: " + name);
+        return "welcome";
+    }
+
+    //http://localhost:8080/product/categoryId/2/categoryName/computer
+    @GetMapping("categoryId/{cateid}/categoryName/{cateName}")
+    public String getProducts(@PathVariable String cateid, @PathVariable("cateName") String name){
+        System.out.println("cateId: " + cateid + " , cateName: " + name);
         return "welcome";
     }
 
