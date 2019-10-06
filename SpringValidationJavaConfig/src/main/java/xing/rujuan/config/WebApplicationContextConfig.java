8956e81248d4e9bdf5c69f7e55ef5745593620c5
rxing <rujuanxing@gmail.com>
2019-10-05 21:23:27 -0500

    JavaConfig version

---
+++ SpringValidationJavaConfig/src/main/java/xing/rujuan/config/WebApplicationContextConfig.java
@@ -0,0 +1,45 @@
+package xing.rujuan.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan({"xing.rujuan"})
+public class WebApplicationContextConfig implements WebMvcConfigurer {
+
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource resource = new ResourceBundleMessageSource();
+        resource.setBasenames("messages", "errorMessages");
+        return resource;
+    }
+
+    @Bean(name="validator")
+    public LocalValidatorFactoryBean validator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+
+    @Override
+    public Validator getValidator() {
+        return validator();
+    }
+}
