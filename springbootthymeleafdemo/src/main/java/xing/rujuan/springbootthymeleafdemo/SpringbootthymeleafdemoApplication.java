f41625e401214c618f3c555c581b1a03f6ca8199
rxing <rujuanxing@gmail.com>
2019-10-10 20:47:43 -0500

    Spring boot Thymeleaf project setup.

---
+++ springbootthymeleafdemo/src/main/java/xing/rujuan/springbootthymeleafdemo/SpringbootthymeleafdemoApplication.java
@@ -0,0 +1,13 @@
+package xing.rujuan.springbootthymeleafdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootthymeleafdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootthymeleafdemoApplication.class, args);
+    }
+
+}
