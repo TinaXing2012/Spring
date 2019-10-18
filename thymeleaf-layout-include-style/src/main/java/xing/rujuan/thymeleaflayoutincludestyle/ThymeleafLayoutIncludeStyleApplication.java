9e9d3195a3292f98aa939de9d38271a85b0820f6
rxing <rujuanxing@gmail.com>
2019-10-17 20:15:15 -0500

    Thymeleaf page layouts - include style

---
+++ thymeleaf-layout-include-style/src/main/java/xing/rujuan/thymeleaflayoutincludestyle/ThymeleafLayoutIncludeStyleApplication.java
@@ -0,0 +1,13 @@
+package xing.rujuan.thymeleaflayoutincludestyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafLayoutIncludeStyleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafLayoutIncludeStyleApplication.class, args);
+    }
+
+}
---
+++ thymeleaf-layout-include-style/src/test/java/xing/rujuan/thymeleaflayoutincludestyle/ThymeleafLayoutIncludeStyleApplicationTests.java
@@ -0,0 +1,13 @@
+package xing.rujuan.thymeleaflayoutincludestyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafLayoutIncludeStyleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
