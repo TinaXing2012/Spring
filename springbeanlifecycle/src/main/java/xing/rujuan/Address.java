a86ec0f2d5783a53da5b96ebc15904bc64a96751
rXing <rxing@mum.edu>
2019-11-01 17:20:00 -0500

    Spring Bean lifecycle 1. Creation - BeanPostProcessor Interface

---
+++ springbeanlifecycle/src/main/java/xing/rujuan/Address.java
@@ -0,0 +1,7 @@
+package xing.rujuan;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Address {
+}
---
+++ springbeanlifecycle/src/main/java/xing/rujuan/CustomBeanPostProcessor.java
@@ -0,0 +1,21 @@
+package xing.rujuan;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomBeanPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("postProcessBeforeInitialization...." + beanName);
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("postProcessAfterInitialization..." + beanName);
+        return bean;
+    }
+}
