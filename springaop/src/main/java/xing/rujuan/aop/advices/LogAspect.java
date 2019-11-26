999bacd9522eca579a06591bd765c0617420bfbf
rxing <rxing@mum.edu>
2019-11-25 23:22:50 -0600

    Advice Type - @JoinPoint

--- springaop/src/main/java/xing/rujuan/aop/advices/LogAspect.java
+++ springaop/src/main/java/xing/rujuan/aop/advices/LogAspect.java
@@ -1,5 +1,6 @@
 package xing.rujuan.aop.advices;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -9,13 +10,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class LogAspect {
 
-    @Before("execution(* *(..))")
-    public void logBefore(){
-        System.out.println("Log before executing....");
+    @Before("execution(* xing.rujuan.aop.advices.AccountService.*(..))")
+    public void logBefore(JoinPoint joinPoint){
+        System.out.println("Log before executing...." + joinPoint.getSignature().getName());
+        System.out.println(joinPoint.getTarget().getClass().getName());
+        System.out.println("Args: " + joinPoint.getArgs()[0]);
+        System.out.println(joinPoint.getThis().getClass().getName());
     }
 
     @After("execution(* xing.rujuan.aop.advices.CustomerService.*(..))")
-    public void logAfter(){
+    public void logAfter(JoinPoint joinPoint){
         System.out.println("Log After executing---");
     }
 }
