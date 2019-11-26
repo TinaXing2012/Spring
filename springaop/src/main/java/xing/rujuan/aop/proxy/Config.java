3e3110365c3e624701a48c2a393437aa525f9157
rxing <rxing@mum.edu>
2019-11-25 19:03:10 -0600

    removed from proxy package to terms package

--- springaop/src/main/java/xing/rujuan/aop/proxy/Config.java
+++
@@ -1,9 +0,0 @@
-package xing.rujuan.aop.proxy;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("xing.rujuan.aop.proxy")
-public class Config {
-}
--- springaop/src/main/java/xing/rujuan/aop/proxy/CustomerService.java
+++
@@ -1,6 +0,0 @@
-package xing.rujuan.aop.proxy;
-
-public interface CustomerService {
-
-    void doSomething();
-}
